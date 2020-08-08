fb99ad7bfc28344cd02da415a8538c9decbcb918
Stoa <542950749@qq.com>
2020-08-08 21:52:00 +0800

    mac

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-3">
-                <div class="list-group">
-                    <a href="{{ route('upload.mac') }}" class="list-group-item list-group-item-action">
-                        上传MAC表
-                    </a>
-                    <a href="{{ route('upload.snkey') }}" class="list-group-item list-group-item-action">上传SN&密钥表</a>
-                    <a href="{{ route('upload.pcba') }}" class="list-group-item list-group-item-action">上传PCBA表</a>
-                </div>
+                @include('common._left')
             </div>
             <div class="col-md-9">
                 <div class="card">
